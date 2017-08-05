ae5b46064c997711c206f2ae67058fa817070c26
zc <zachcaceres@gmail.com>
2017-08-05 14:41:55 -0400

    format

--- src/sorter.js
+++ src/sorter.js
@@ -5,46 +5,66 @@ var sourceJSON = [
     title: 'Mailchimp configuration',
     price: 120,
     time: 3,
-    tags: ['mailchimp', 'setup', 'easy']
+    tags: ['mailchimp', 'setup', 'easy'],
+    people: {
+      client: 'John Smith',
+      expert: 'Charlie'
+    }
   },
   {
     title: 'Error in mailing list',
     price: 20,
     time: 1,
-    tags: ['mailchimp', 'bugs']
+    tags: ['mailchimp', 'bugs'],
+    people: {
+      client: 'Great Consulting',
+      expert: 'Charlie'
+    }
   },
   {
     title: 'Newsletter Setup Mailchimp',
     price: 100,
     time: 5,
-    tags: ['mailchimp', 'squarespace']
+    tags: ['mailchimp', 'squarespace'],
+    people: {
+      client: 'Ashley Koff',
+      expert: 'Charlie'
+    }
   },
   {
     title: 'Broken Signup form',
     price: 120,
     time: 3,
-    tags: ['mailchimp', 'setup', 'easy']
+    tags: ['mailchimp', 'setup', 'easy'],
+    people: {
+      client: 'Do You Remember',
+      expert: 'Charlie'
+    }
   },
   {
     title: 'Button CSS',
     price: 40,
     time: 2,
-    tags: ['css', 'design', 'wordpress']
+    tags: ['design', 'wordpress'],
+    people: {
+      client: 'Jane Smith',
+      expert: 'Charlie'
+    }
   },
 ];
 
 function setup(sourceJSON) {
   var stringJSON = JSON.stringify(sourceJSON);
-  var jsonArr = flatten(JSON.parse(stringJSON));
-  var trie = createTrie();
-  jsonArr.forEach((obj) => {
-    Object.keys(obj).forEach((key) => {
-      var val = obj[key].toString().toLowerCase();
-      // console.log(obj[key], val);
-      trie.insert(val, obj);
-    });
-  });
-  return trie;
+  sourceJSON = flatten(JSON.parse(stringJSON));
+  // var trie = createTrie();
+  // jsonArr.forEach((obj) => {
+  //   Object.keys(obj).forEach((key) => {
+  //     var val = obj[key].toString().toLowerCase();
+  //     // console.log(obj[key], val);
+  //     trie.insert(val, obj);
+  //   });
+  // });
+  // return trie;
 }
 
 function flatten(json) {
@@ -77,6 +97,20 @@ function flattenArr(json) {
   return newArray;
 }
 
+function keysContainInput(obj, input) {
+  // console.log('obj', obj)
+
+  return Object.keys(obj).some((el) => {
+    return el.toString().toLowerCase().includes(input);
+  });
+}
+
+function valsContainInput(obj, input) {
+  return Object.keys(obj).some((el) => {
+    return obj[el].toString().toLowerCase().includes(input);
+  });
+}
+
 function filterUserInput(e) {
   var currVal = e.target.value.toLowerCase();
   while (layout.firstChild) {
@@ -85,11 +119,20 @@ function filterUserInput(e) {
   if (!e.target.value) {
     return;
   }
-  var resultArr = trie.autoComplete(currVal);
+  var resultArr = sourceJSON.filter(obj => {
+    return keysContainInput(obj, currVal) || valsContainInput(obj, currVal)
+  })
+  // var resultArr = trie.autoComplete(currVal);
   resultArr.forEach((res) => {
     var resultStringified = (typeof res !== 'string' && typeof res !== 'number') ? JSON.stringify(res) : res;
     var p = document.createElement('p');
-    p.innerHTML = resultStringified;
+    var h2 = document.createElement('h2');
+    var h3 = document.createElement('h3');
+    p.innerHTML = 'Tags: ' + res.tags;
+    h2.innerHTML = res.title;
+    h3.innerHTML = 'Price: ' + res.price;
+    layout.appendChild(h2);
+    layout.appendChild(h3);
     layout.appendChild(p);
   });
 }
