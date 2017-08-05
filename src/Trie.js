216f01031afb7650133610eaaed108c3eb136236
Michael Noronha <michaeltnoronha@gmail.com>
2017-08-05 12:30:52 -0400

    Trie implementation

---
+++ src/Trie.js
@@ -0,0 +1,71 @@
+// Slightly modified from implementation by yonatanmn
+// [https://gist.github.com/yonatanmn/bff1b8dd80fc549da08d209f8917faa4]
+//
+// Modifications:
+//  - Completions are not excluded when they are first finished
+//  - The output array is an array of objects rather than of strings/keys
+
+function goto(o,wp){
+    if(!wp.length) {
+        return o;
+    }
+
+    var firstLetter = wp[0];
+    var point = o[firstLetter];
+
+    return point ? goto(point,wp.slice(1)) : {};
+}
+
+var objSym = Symbol.for('obj');
+
+var TrieProto = {
+    insert: function(w,obj){
+        var point = this.tree;
+
+        w.split('').forEach(function(e,i){
+            if(!point[e]) {
+              point[e] = {};
+            }
+            point = point[e];
+            if(w.length - 1 === i) {
+                point[objSym] = obj;
+            }
+        });
+    },
+
+    autoComplete: function(wp){
+        var point = goto(this.tree, wp);
+        var stack = [];
+
+        // We can check if we've completed a word in constant time
+        if(point[objSym]) {
+            stack.push(point[objSym]);
+        }
+
+        function reduceObjToArr(o, trace) {
+            for(var k in o) {
+                if(o[k][objSym]) {
+                    stack.push(o[k][objSym]);
+                    // stack.push(trace + k);
+                }
+                reduceObjToArr(o[k], trace + k);
+            }
+        }
+
+        reduceObjToArr(point, '');
+
+        return stack;
+    }
+};
+
+var TrieDesc = {
+    tree:{
+        value:{},
+        enumerable:true,
+    }
+};
+
+function createTrie(){
+    return Object.create(TrieProto,TrieDesc);
+}
+
